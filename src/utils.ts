9dc7b85cb3a1b77c9649cb15a0d26ce3928c6e4a
bmarian <33843056+bmarian@users.noreply.github.com>
2025-02-28 21:37:31 +0200

    .gitignore

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-const numberFormater = new Intl.NumberFormat("en-US", {
-  maximumFractionDigits: 2,
-})
-export const scientificFormat = (value: number): string => {
-  if (value < 1000000) return numberFormater.format(value)
-  return value.toExponential(2)
-}
